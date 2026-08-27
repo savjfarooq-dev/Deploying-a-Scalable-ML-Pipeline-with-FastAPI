91bd1d35a692cfa214d2713b53372bcc03afdfc6
Savannah F <savjfarooq@gmail.com>
2026-08-26 20:24:33 -0400

    Created three unit tests and uploaded unit_test screenshot

--- test_ml.py
+++ test_ml.py
@@ -1,28 +1,45 @@
 import pytest
-# TODO: add necessary import
 
-# TODO: implement the first test. Change the function name and input as needed
-def test_one():
-    """
-    # add description for the first test
-    """
-    # Your code here
-    pass
+# Add additional required imports
+from ml.data import apply_label
+from sklearn.ensemble import RandomForestClassifier
+from ml.model import compute_model_metrics, train_model
 
+# Implement first unit test. 
+def test_apply_label():
+    """
+    Test that apply_label returns the expected salary label.
+    """
+    prediction = [1]
 
-# TODO: implement the second test. Change the function name and input as needed
-def test_two():
-    """
-    # add description for the second test
-    """
-    # Your code here
-    pass
+    result = apply_label(prediction)
+
+    assert result == ">50K"
 
 
-# TODO: implement the third test. Change the function name and input as needed
-def test_three():
+# Implement second unit test.
+def test_train_model():
     """
-    # add description for the third test
+    Test that train_model returns a RandomForestClassifier.
     """
-    # Your code here
-    pass
+    X_train = [[1, 2], [2, 3], [3, 4], [4, 5]]
+    y_train = [0, 0, 1, 1]
+
+    model = train_model(X_train, y_train)
+
+    assert isinstance(model, RandomForestClassifier)
+
+
+# Implement third unit test. 
+def test_compute_model_metrics():
+    """
+    Test that compute_model_metrics returns expected metric values.
+    """
+    y = [1, 1, 0, 0]
+    preds = [1, 0, 0, 0]
+
+    precision, recall, fbeta = compute_model_metrics(y, preds)
+
+    assert precision == pytest.approx(1.0)
+    assert recall == pytest.approx(0.5)
+    assert fbeta == pytest.approx(2 / 3)
